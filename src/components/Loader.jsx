084235b5c3282a73f45d8e210024d05b79ab0e94
arifshaikhvk25 <arifshaikhvk25@gmail.com>
2023-10-24 15:25:03 +0530

    completed Hero Section

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-
+import { progress } from "framer-motion";
+import {Html, useProgress} from "@react-three/drei";
 const Loader = () => {
+  const {progress} = useProgress()
   return (
-    <div>Loader</div>
+    <Html>
+      <span className="canvas-load"></span> 
+      <p style={{fontSize:30, color:"white", fontWeight: 400, marginTop:"8rem", width:"2vw" }  }>
+      {progress.toFixed(2)}%
+      </p>
+    </Html>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-const Computers = () => {
+import CanvasLoader from "../Loader";
+
+const Computers = ({ mobileResponsive }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+  // console.log(computer);
+
   return (
-    <div>Computers</div>
-  )
-}
+    <mesh>
+      {/* <hemisphereLight intensity={1} groundColor="green" /> */}
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, -10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={mobileResponsive ? 0.4 : 0.55}
+        position={mobileResponsive ? [0, -3, -1.1] : [0, -3.9, -1.1]}
+      />
+    </mesh>
+  );
+};
 
-export default Computers
+const ComputersCanvas = () => {
+  const [mobileResponsive, setmobileResponsive] = useState(false);
+
+  useEffect(() => {
+    const mobileMediaQuery = window.matchMedia("(max-width: 500px)");
+    setmobileResponsive(mobileMediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setmobileResponsive(event.matches);
+    };
+    mobileMediaQuery.addEventListener("change", handleMediaQueryChange);
+    return () => {
+      mobileMediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          rotation={[-3.71, -16.2, -1.0]}
+        />
+        <Computers mobileResponsive={mobileResponsive} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
+// export default Computers;
