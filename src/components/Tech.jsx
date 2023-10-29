34edc4f6af76f7ce1eebc9eeff61b58be3715f28
arifshaikhvk25 <arifshaikhvk25@gmail.com>
2023-10-29 16:16:20 +0530

    added tech stack icons

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { BallCanvas } from "./canvas"
+import { hocSectionWrapper } from "../hoc"
+import { technologies } from "../constants"
+
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className="flex flex-row flex-wrap justify-center gap-10" >
+      {
+        technologies.map((eachtechnology, index) =>{
+          return(
+            <div className=" w-28 h-28" key={index} > 
+              <BallCanvas icon={eachtechnology.icon} />
+            </div>
+          )
+        })
+      }
+
+    </div>
   )
 }
 
